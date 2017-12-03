d28bc74d44f8d208079e368ac897511a20972f7f
chiew <chiewminhai@gmail.com>
2017-12-03 16:00:18 +0800

    fix

--- K_Nearest_Neighbors/KNN.py
+++ K_Nearest_Neighbors/KNN.py
@@ -2,10 +2,10 @@
 # @Author  : chiew
 # @File    : KNN.py
 
-import numpy as np
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 
 iris = datasets.load_iris()
@@ -25,3 +25,18 @@ knn.fit(X_train, y_train)
 print(knn.predict(X_test))
 print(y_test)
 print(knn.score(X_test, y_test))
+
+
+k_range = range(1,31)
+k_scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, iris_X, iris_y, cv=10, scoring='accuracy')
+    k_scores.append(scores.mean())
+
+print(k_scores)
+
+plt.plot(k_range, k_scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Cross-Validated Accuracy')
+plt.show()
